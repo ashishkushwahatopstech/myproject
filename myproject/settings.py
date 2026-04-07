f0cddbd48c9f348bde1f3e6b40ef8f63d0eb0460
ashishkushwahatopstech <ashishkushwaha.topstechnology@gmail.com>
2026-04-07 10:10:31 +0530

    initial commit 2

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,3 +135,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = os.environ.get("DEBUG") == "True"
